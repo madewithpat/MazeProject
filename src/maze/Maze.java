9d4fc77a98f441435ebd21a111bd5ba597e1eee1
Patrick <info@patrickoutler.io>
2017-03-08 16:34:51 -0500

    Converted to arrayList, began work on takeStep()

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -1,5 +1,7 @@
 package maze;
 
+import java.util.ArrayList;
+
 public class Maze {
 	private char direction; //'n','e','w','s'
     private int r;  // x position of the mouse
@@ -7,43 +9,55 @@ public class Maze {
     private boolean exitFound = false;
 
 
-
+    private ArrayList<ArrayList<Object>> formattedMaze = new ArrayList<>();
+    
+    
+    private ArrayList<Move> path = new ArrayList<>();
+    private ArrayList<Move> illegalMoves = new ArrayList<>();
 
 
     public Maze(int[][] arrMaze) {
         this.direction = 'n';
-        this.r = maze.size() - 1;
         this.c = 0;
+        this.r = 0;
+        
+        
+        for (int i = 0; i < arrMaze.length; i++) {
+        	ArrayList<Object> mazeRow = new ArrayList<>();
+        	
+        	for (int j = 0; j < arrMaze[i].length; j++) {
+        		if (arrMaze[i][j] == 1) {
+        			mazeRow.add(" ");
+        		}
+        		else if (arrMaze[i][j] == 0) {
+        			mazeRow.add("#");
+        		}
+        	}
+        	
+        	this.formattedMaze.add(mazeRow);
+        }
      }
 
     //Prints out the maze without solution
     public void displayMaze() {
-    	for (int i = 0; i < arrMaze.length;i++) {
-    		for (int z = 0; z < arrMaze[i].length; z++) {
-    		
-    			if (1 == arrMaze[i][z]) {
-    				System.out.print(" ");
-    			}
-    			else if (0 == arrMaze[i][z]) {
-    				System.out.print("#");
-    			}
-    		}
+    	for (int i = 0; i < this.formattedMaze.size(); i++) {
+    		System.out.print("\n" + this.formattedMaze.get(i).toString());
+    	}
     	
-    	System.out.println();
-    	
-
+    	formattedMaze.forEach
     }
 
     //displays the Maze with the path taken
     public void displayPath() {
+    	
+    	//arrMaze[this.r][this.c] = '@';
 
     }
 
 
     public boolean takeStep() {
         //complete the code here
-        // Create a collection of coordinate pairs availableMoves
-        // Store the chars of each direction in availableMoves
+        
         // Iterate through availableMoves
         //      if (availableMoves[i] == '~') {
         //          temp = availableMoves[0];
@@ -65,6 +79,35 @@ public class Maze {
         //          illegalMoves.add(currentPosition);
         //          currentPosition = ' ';
         //          move${direction of availableMoves[0]}
+    	
+    	// Create a collection of coordinate pairs availableMoves
+    	ArrayList<char> availableMoves = new ArrayList<>();
+    	
+    	// indices correspond to direction, 0 = N, 1 = E, etc.
+    	availableMoves.add(new Move(r - 1, c));
+    	availableMoves.add(new Move(r, c + 1));
+    	availableMoves.add(new Move(r + 1, c));    	
+    	availableMoves.add(new Move(r, c - 1));
+    	
+    	while (availableMoves.contains)
+    	
+    	for (int i = 0; i < availableMoves.size(); i++) {
+    		char test = formattedMaze[availableMoves.get(1).x][availableMoves.get(1).y];
+    		
+    		if (test == "#") {
+    			availableMoves.set(i, null);
+    		}
+    		
+    		if (illegalMoves.contains(moves[i])) {
+    			moves[i] = null;
+    		}
+    		
+    		if (test == "~") {
+    			moves[i] = null;
+    		}
+    	}
+    	
+    
 
         return isAnExit();
     }
@@ -109,4 +152,19 @@ public class Maze {
         //complete the code here
 
     }
+    
+    class Move {
+    	protected int x;
+    	protected int y;
+    	
+    	protected Move(int x, int y) {
+    		this.x = x;
+    		this.y = y;
+    	}
+    }
+    
+//    class Stack {
+//    	private ArrayList<Object> list = new ArrayList<>();
+//    	
+//    }
 }
