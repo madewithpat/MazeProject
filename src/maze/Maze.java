150afd68ca08507379e649e7b883c70bd2015959
Patrick <info@patrickoutler.io>
2017-03-09 18:03:52 -0500

    Completed takeStep(), added methods to Maze and MazePoint

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -8,13 +8,11 @@ public class Maze {
     private int c;  //y position of the mouse
     private boolean exitFound = false;
 
-
-    private ArrayList<ArrayList<String>>theMaze = new ArrayList<>();
+    private ArrayList<ArrayList<String>>theMaze = new ArrayList<>(); // Formatted maze    
     
-    
-    private ArrayList<ArrayList<String>> thePath = new ArrayList<>();
-    private ArrayList<MazePoint> illegalMoves = new ArrayList<>();
-    private MazePointStack moves = new MazePointStack();
+    private ArrayList<ArrayList<String>> thePath = new ArrayList<>(); // Formatted maze with path
+    private ArrayList<MazePoint> illegalMoves = new ArrayList<>(); // Persistent list of invalid moves
+    private MazePointStack stepsTaken = new MazePointStack(); // List of correct moves taken
     
 
     public Maze(int[][] arrMaze) {
@@ -57,68 +55,119 @@ public class Maze {
 
     public boolean takeStep() {
         //complete the code here
+    	// Placeholder for next move to make
+    	MazePoint nextMove;
             	
     	// Create a collection of coordinate pairs availableMoves
     	ArrayList<MazePoint> availableMoves = new ArrayList<>();
     	
-    	// Fill availableMoves list with values.
-    	availableMoves.add(new MazePoint(r - 1, c));
-    	availableMoves.add(new MazePoint(r, c + 1));
-    	availableMoves.add(new MazePoint(r + 1, c));
-    	availableMoves.add(new MazePoint(r, c - 1));
-    	    	    	
-    	for (MazePoint temp : availableMoves) {    		
-    		if (temp.value == "#") {
-//    			availableMoves.set(availableMoves.indexOf(temp), null);
-    			availableMoves.remove(availableMoves.indexOf(temp));
-    		}
-    		else if (temp.value == "~") {
-    			availableMoves.remove(availableMoves.indexOf(temp));
-//    			availableMoves.set(availableMoves.indexOf(temp), null);
-    		}
-    		else if (temp.isIllegal()) {
-    			availableMoves.remove(availableMoves.indexOf(temp));
-//    			availableMoves.set(availableMoves.indexOf(temp), null);
-    		}
-    	}
+    	// Fill availableMoves list with a MazePoint for each possible direction
+    	availableMoves.add(new MazePoint(r - 1, c, 'n'));
+    	availableMoves.add(new MazePoint(r, c + 1, 'e'));
+    	availableMoves.add(new MazePoint(r + 1, c, 's'));
+    	availableMoves.add(new MazePoint(r, c - 1, 'w'));    	
+    	    	
+    	/**
+    	 * 
+    	 * Thinking Out Loud Here!
+    	 * 
+    	 * This could be adjusted based on position, for efficiency
+    	 * if current position is on the left side of the maze, moves to the east should be prioritized.
+    	 * if current position is on the bottom, moves to the north should be priority.
+    	 * if current position is farther south than west, then moves to the north are higher priority than moves to the east
+    	 * which also means that moves to the south would be lower priority than moves to the west    	 * 
+    	 * 
+    	 */
     	
-    	if (availableMoves.size() == 0) {
-    		MazePoint prev = moves.peek();
-    		illegalMoves.add(new MazePoint(r, c));
-    		
-    		switch (this.r - prev.x) {
-    		case (1):
-    			direction = 'n';
-    			break;
-    		case (-1):
-    			direction = 's';
-    			break;
-    		case (0):
-    			if (c > prev.y) {
-    				direction = 'w';
-    			}
-    			else if (c < prev.y) {
-    				direction = 'e';
-    			}
-    			break;
-    		}
+    	// Sort availableMoves so that the order is always clockwise, but beginning with the current direction
+    	// e.g., if direction == 's', this will reorganize availableMoves so that the MazePoints are in order of (S, W, N, E)    	
+    	this.sortMoves(availableMoves);
+    	
+    	// Filter out invalid and/or illegal moves
+    	this.filterMoves(availableMoves);
+    	
+    	// initialize nextMove based on the remaining availableMoves
+    	if (availableMoves.size() > 0) {
+    		// If there are moves available, nextMove becomes the first of those remaining moves
+    		nextMove = availableMoves.get(0);
+    		// Then push the nextMove onto our stack of stepsTaken
+    		stepsTaken.push(nextMove);
     	}
     	else {
-    		MazePoint next = availableMoves.get(0);
-    		
-    		switch (this.r - next.x) {
-    		case (1)
-    		}
+    		// If there are no remaining moves, pop the previous move off the top of stepsTaken
+    		nextMove = stepsTaken.pop();
+    		// Add that move to the list of illegalMoves, so we don't make the same move again
+    		illegalMoves.add(nextMove);
+    		/**
+    		 * The direction of the move on the top of the stack references the direction FROM the previous point TO the current position
+    		 * In order to take a step backwards, we invert the direction
+    		 * --AFTER adding it to the list of illegalMoves--
+    		 * this allows us to use one switch statement to call a move method
+    		 */
+    		nextMove.invertDirection();
     	}
     	
-    	
+    	// Select direction to move based on nextMove
+    	switch (nextMove.direction) {
+    	case ('n'):
+    		moveNorth();
+    		break;
+    	case ('e'):
+    		moveEast();
+    		break;
+    	case ('s'):
+    		moveSouth();
+    		break;
+    	case ('w'):
+    		moveWest();
+    		break;
+    	}    			
+    		    	
         return isAnExit();
     }
-
+    
+    private void sortMoves(ArrayList<MazePoint> list) {
+    	int sortCounter = 0;
+    	
+    	switch (this.direction) {
+    	case ('n'):
+    		sortCounter = 0;
+    		break;
+    	case ('e'):
+    		sortCounter = 1;
+    		break;
+    	case ('s'):
+    		sortCounter = 2;
+    		break;
+    	case ('w'):
+    		sortCounter = 3;
+    		break;
+    	}
+    	
+    	while (sortCounter > 0) {
+    		MazePoint temp = list.get(0);
+    		list.remove(0);
+    		list.add(temp);
+    	}
+    }
+    
+    private void filterMoves(ArrayList<MazePoint> list) {
+    	for (MazePoint temp : list) {    		
+    		if (temp.value == "#") {
+    			list.remove(list.indexOf(temp));
+    		}
+    		else if (temp.value == "~") {
+    			list.remove(list.indexOf(temp));
+    		}
+    		else if (temp.isIllegal()) {
+    			list.remove(list.indexOf(temp));
+    		}
+    	}
+    }
+        
     private void moveNorth() {
         //complete the code here
         thePath.get(r).set(c, "~");
-        moves.push(new MazePoint(r, c));
         r -= 1;
         thePath.get(r).set(c, "@");
     }
@@ -160,11 +209,13 @@ public class Maze {
     class MazePoint implements Comparable<MazePoint>{
     	protected int x;
     	protected int y;
+    	protected char direction;
     	protected String value;
     	
-    	protected MazePoint(int x, int y) {
+    	protected MazePoint(int x, int y, char direction) {
     		this.x = x;
-    		this.y = y;    		
+    		this.y = y;   
+    		this.direction = direction;
     		this.value = theMaze.get(x).get(y);
     	}
     	
@@ -176,6 +227,23 @@ public class Maze {
     		}
     		return false;
     	}
+    	
+    	protected void invertDirection() {
+        	switch (this.direction) {
+        	case ('n'):
+        		this.direction = 's';
+        	break;
+        	case ('e'):
+        		this.direction = 'w';
+        	break;
+        	case ('s'):
+        		this.direction = 'n';
+        	break;
+        	case ('w'):
+        		this.direction = 'e';
+        	break;
+        	}
+        }
     	
     	@Override
     	public int compareTo(MazePoint point) {
